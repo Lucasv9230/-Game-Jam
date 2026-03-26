866f9089781679c540d3f7447c479c6f9bbce534
Lucas <lucas.vauclin@ynov.com>
2026-03-26 09:17:32 +0100

    add case timer and move timer in case

---
+++ js/timer/timer-facile.js
@@ -0,0 +1,17 @@
+ let time = 60;
+    const timerElement = document.getElementById("timer");
+
+    const interval = setInterval(() => {
+        time--;
+
+        // Affiche le timer à l'écran
+        timerElement.textContent = "Temps restant : " + time;
+
+        // Quand le timer arrive à 0 → victoire
+        if (time <= 0) {
+            clearInterval(interval);
+
+            alert("VICTOIRE : L'operation est un succes !");
+            window.location.href = "../index.html"; // Retour au menu
+        }
+    }, 1000);
